2a864e489e6960f103dc9e915b3ea8ff0c4d5089
Chalwk77 <jericho.crosby227@gmail.com>
2018-02-23 20:42:40 +1300

    .

---
+++ scenes/debug.lua
@@ -0,0 +1,57 @@
+-----------------------------------------------------------------------------------------
+-- debug.lua
+-- (c) 2018, Velocity by Jericho Crosby <jericho.crosby227@gmail.com>
+--
+-----------------------------------------------------------------------------------------
+local show_fps = false
+
+showFPS = function()
+
+    local prevTime = 0
+    local curTime = 0
+    local dt = 0
+    local fps = 50
+    local mem = 0
+
+    local xPos = display.contentCenterX
+    local yPos = display.contentCenterX + display.contentCenterY - 90
+
+    local underlay = display.newRect(0, 0, 300, 20, 12)
+    underlay.x = xPos
+    underlay.y = yPos
+    underlay:setFillColor(0, 0, 0, 128)
+
+    local displayInfo = display.newText("FPS: " .. fps .. " - Memory: ".. mem .. "mb", 0, 0, native.systemFontBold, 15 )
+    displayInfo.x = xPos
+    displayInfo.y = yPos
+    displayInfo:setFillColor(255, 0, 0, 1)
+    displayInfo.alpha = 1
+
+    local function updateText()
+        curTime = system.getTimer()
+        dt = curTime - prevTime
+        prevTime = curTime
+        fps = math.floor(1000 / dt)
+        mem = system.getInfo("textureMemoryUsed") / 1000000
+        if fps > 60 then fps = 60 end
+        displayInfo.text = "FPS: " .. fps .. " - Memory: ".. string.sub(mem, 1, string.len(mem) - 4) .. "mb"
+        underlay:toFront()
+        displayInfo:toFront()
+    end
+
+    Runtime:addEventListener("enterFrame", updateText)
+end
+
+
+debug = function(debug_text)
+    if debug_text ~= nil then
+        local debug_message = display.newText(tostring(debug_text), 0, 0, native.systemFontBold, 15 )
+        debug_message.x = display.contentCenterX
+        debug_message.y = display.contentCenterY - 80
+        debug_message:setFillColor(255, 0, 0, 1)
+        debug_message.alpha = 1
+        print("Debug: " .. tostring(debug_text))
+    end
+end
+
+if (show_fps == true) then showFPS() end
--- scenes/menu.lua
+++ scenes/menu.lua
@@ -5,16 +5,42 @@
 -----------------------------------------------------------------------------------------
 local composer = require( 'composer' )
 local scene = composer.newScene()
+local widget = require( "widget" )
+require('scenes.debug')
 
 function scene:create( event )
     local sceneGroup = self.view
-    local function setUpDisplay(group)
-        local background = display.newImage(group, "images/menu_background.png")
+    local function setUpDisplay(sceneGroup)
+        -- draw menu background
+        local background = display.newImage(sceneGroup, "images/backgrounds/menu_background.png")
         background.xScale = (0.5 * background.contentWidth) / background.contentWidth
         background.yScale = background.xScale
         background.x = display.contentCenterX
         background.y = display.contentCenterY
         background:scale(0.4, 0.4)
+        -- create menu buttons
+        local x, y = -1.75, 0
+        local spacing = 100
+        for buttonID = 1, 2 do
+            local buttons = widget.newButton ({
+                defaultFile = 'images/buttons/button_' .. buttonID .. '.png',
+                overFile = 'images/buttons/button_' .. buttonID .. '_pressed.png',
+                width = 164, height = 54,
+                x = x * spacing + 250,
+                y = 450 + y * spacing,
+                onRelease = function()
+                    if (buttonID == 1) then
+                        -- gotoScene 1
+                    elseif (buttonID == 2) then
+                        -- gotoScene 2
+                    end
+                end
+            })
+            buttons:scale(0.8, 0.5)
+            sceneGroup:insert(buttons)
+            local button_spacing = 1.7
+            x = x + button_spacing
+        end
     end
     setUpDisplay(sceneGroup)
 end
